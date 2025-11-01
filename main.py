dafa750907d8b3ee403935634fc2c5621a12fcc7
Devesh Saini <deveshsaini369@gmail.com>
2025-11-01 21:15:41 +0530

    Basic structure done, bug fixes are required.

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # Data -> Embeddings -> Store -> Retrieve
 
-import requests
-import chromadb
 from langchain.tools import tool
 from langchain_chroma import Chroma
 from langchain.agents import create_agent
@@ -31,8 +29,8 @@ splittedDoc = textSplitter.split_documents(docs)
 
 embeddings = OllamaEmbeddings(model="mistral:instruct")
 
-vector1 = embeddings.embed_query(splittedDoc[0].page_content)
-vector2 = embeddings.embed_query(splittedDoc[1].page_content)
+#vector1 = embeddings.embed_query(splittedDoc[0].page_content)
+#vector2 = embeddings.embed_query(splittedDoc[1].page_content)
 
 vectorDb = Chroma(
             collection_name="Python-Documentation",
@@ -43,3 +41,32 @@ vectorDb = Chroma(
 vectorDb.add_documents(documents=splittedDoc)
 
 
+# Creating tools.
+
+@tool
+def retrieveContext(query: str):
+    retrievedInfo = vectorDb.similarity_search_with_score(query, k=2)
+    return retrievedInfo
+
+tools = [retrieveContext]
+prompt = (
+            """
+            You are Alfred from the movie 'The Batman' that was released in 
+            2022. You have a tool that helps in retrieving information about
+            Python programming language.
+            Your task is to help with any queries that the user has. Treat
+            the user like they are Bruce Wayne to you.
+            """
+        )
+
+model = "mistral:instruct"
+
+Alfred = create_agent(model, tools, system_prompt=prompt)
+
+Query = input("=>")
+
+for event in Alfred.stream(
+        {"messages": [{"role": "user", "content": Query}]},
+        stream_mode="values"
+        ):
+    event["messages"][-1].pretty_print()
